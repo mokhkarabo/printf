f52616d024d10909812435b1402817979bd95b20
mokhkarabo <mokhkarabo>
2023-08-24 04:27:33 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,11 +1,23 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+/**
+ * print_buffer - Prints the contents of the buffer if it exists
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add the next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+	{
+		write(1, &buffer[0], *buff_ind);
+		*buff_ind = 0;
+	}
+}
 
 /**
  * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * @format: Format string
+ * Return: Number of printed chars
  */
 int _printf(const char *format, ...)
 {
@@ -25,8 +37,9 @@ int _printf(const char *format, ...)
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
+			{
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
+			}
 			printed_chars++;
 		}
 		else
@@ -36,7 +49,7 @@ int _printf(const char *format, ...)
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
-			++i;
+			i++;
 			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
@@ -50,17 +63,4 @@ int _printf(const char *format, ...)
 	va_end(list);
 
 	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
 }
